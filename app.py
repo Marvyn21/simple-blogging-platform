a88297bc02329c09c37a39edb1497b324df8d3b2
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-06-13 11:54:49 +0300

    layout

--- app.py
+++ app.py
@@ -1,39 +1,91 @@
-from flask import Flask, render_template, request, redirect
-import sqlite3
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
-database = 'blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 @app.route('/')
 def index():
-    conn = sqlite3.connect(database)
-    cursor = conn.cursor()
-    cursor.execute('SELECT posts.id, title, content, username FROM posts INNER JOIN users ON posts.user_id = users.id')
-    posts = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
-@app.route('/signup', methods=['GET, POSTS'])
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    #TODO: IMPLEMENT LOGIC
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return render_template('signup.html', error='Username already exists')
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/login')
+
     return render_template('signup.html')
 
-@app.route('/login', methods=['GET, POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Implement user login logic
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            session['user_id'] = user.id  # Store the user's session or token for authentication
+            return redirect('/')
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
     return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
-    # Implement user logout logic
+    session.pop('user_id', None)  # Remove the user's session or token
     return redirect('/')
+
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
-    #Implement post creation logic
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        author = 'John'  # Replace with the actual author, either from the session or the currently logged-in user
+
+        new_post = Post(title=title, content=content, author=author)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/')
+
     return render_template('create.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
